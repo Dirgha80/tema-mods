bc9941b1708f401236a9ae239c2b7dbb551621c4
Dirgha80 <156698511+Dirgha80@users.noreply.github.com>
2025-10-03 01:54:53 +0700

    Update orion.lua

--- luci-theme-orion/luasrc/model/cbi/orion.lua
+++ luci-theme-orion/luasrc/model/cbi/orion.lua
@@ -1,32 +1,32 @@
 local m, s, o
 
-m = Map("orion", "Orion 主题配置")
+m = Map("orion", "Orion Konfigurasi Tema")
 
-s = m:section(TypedSection, "theme", "主题设置")
+s = m:section(TypedSection, "theme", "Pengaturan tema")
 s.anonymous = true
 s.addremove = false
 
-o = s:option(Flag, "navbar", "启用导航栏")
+o = s:option(Flag, "navbar", "Aktifkan bilah navigasi")
 o.default = "1"
 
 -- Navigation bar configuration
-s = m:section(TypedSection, "navbar", "导航栏项目")
+s = m:section(TypedSection, "navbar", "Proyek Bar Navigasi")
 s.anonymous = true
 s.addremove = true
 s.template = "cbi/tblsection"
 
-o = s:option(Value, "name", "名称")
+o = s:option(Value, "name", "nama")
 
-o = s:option(Flag, "enable", "启用")
+o = s:option(Flag, "enable", "nyala")
 o.default = "Enable"
 
-o = s:option(Value, "line", "排序")
+o = s:option(Value, "line", "line")
 o.default = "1"
 
-o = s:option(Flag, "newtab", "新标签页")
+o = s:option(Flag, "newtab", "tab baru")
 o.default = "No"
 
-o = s:option(ListValue, "icon", "图标")
+o = s:option(ListValue, "icon", "icon")
 o:value("resources/icons/navbar/overview.png", "概览")
 o:value("resources/icons/navbar/network.png", "网络")
 o:value("resources/icons/navbar/openclash.png", "Clash")
@@ -35,6 +35,9 @@ o:value("resources/icons/navbar/neko.png", "Neko")
 o:value("resources/icons/navbar/terminal.png", "终端")
 o.default = "resources/icons/navbar/overview.png"
 
+o = s:option(Value, "address", "网址")
+
+return m
 o = s:option(Value, "address", "网址")
 
 return m
